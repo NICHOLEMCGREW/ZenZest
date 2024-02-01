c42cd44600c84cbd6a5acc179a4feb6e61c060ff
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-01 09:37:41 -0600

    connected to database

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,10 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from 'react-router-dom';
+import { useToast } from "@/components/ui/use-toast"
 
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
@@ -20,6 +20,7 @@ import { Loader } from "lucide-react";
 import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+	const { toast } = useToast()
 	const isLoading = false;
 	// 1. Define your form.
 	const form = useForm<z.infer<typeof SignupValidation>>({
@@ -35,7 +36,11 @@ const SignupForm = () => {
 	// 2. Define a submit handler.
 	async function onSubmit(values: z.infer<typeof SignupValidation>) {
 		const newUser = await createUserAccount(values);
-		console.log(newUser)
+		if(!newUser) {
+			return toast({ title: 'Sign up faild. Please try again.' })
+		}
+
+		const session = await signInAccount()
 	}
 
 	return (
