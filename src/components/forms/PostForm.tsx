7ca19f0f333a7db1cfb74aa8923c69bf83273d2b
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-04 08:00:54 -0600

    added likes and saves to PostCard

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,46 +1,72 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import * as z from "zod";
+import { Models } from "appwrite";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "../ui/textarea"
-import FileUploader from "../shared/FileUploader"
+  Button,
+  Input,
+  Textarea,
+} from "@/components/ui";
+import { PostValidation } from "@/lib/validation";
+import { useToast } from "@/components/ui/use-toast";
+import { useUserContext } from "@/context/AuthContext";
+import { FileUploader } from "@/components/shared";
+import { useCreatePost } from "@/lib/react-query/queries";
+// import { Form } from "../ui/form";
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
-const PostForm = ({ post }) => {
-      // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
- 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
+type PostFormProps = {
+    post?: Models.Document;
 }
+
+const PostForm = ({ post }: PostFormProps) => {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  const { user } = useUserContext();
+  
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
+    defaultValues: {
+      caption: post ? post?.caption : "",
+      file: [],
+      location: post ? post.location : "",
+      tags: post ? post.tags.join(",") : "",
+    },
+  });
+  
+   // Query
+   const { mutateAsync: createPost, isPending: isLoadingCreate } =
+   useCreatePost();
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    const newPost = await createPost({
+        ...values, 
+        userId: user.id,
+    })
+
+    if(!newPost) {
+        toast({
+            title: 'Please try again',
+        })
+    }
+
+    navigate('/');
+  };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+      <form
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="flex flex-col gap-9 w-full max-w-5xl">
         <FormField
           control={form.control}
           name="caption"
@@ -54,8 +80,7 @@ const PostForm = ({ post }) => {
             </FormItem>
           )}
         />
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+
         <FormField
           control={form.control}
           name="file"
@@ -72,8 +97,7 @@ const PostForm = ({ post }) => {
             </FormItem>
           )}
         />
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+
         <FormField
           control={form.control}
           name="location"
@@ -81,18 +105,17 @@ const PostForm = ({ post }) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type='text' className='shad-input' />
+                <Input type='text' className='shad-input' {...field} />
               </FormControl>
               <FormMessage  className="shad-form_message"/>
             </FormItem>
           )}
         />
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+
         <FormField
           control={form.control}
           name="tags"
-          render={({ field }) => (
+          render={({...field }) => (
             <FormItem>
               <FormLabel className="shad-form_label">Add Tags (seperated by comma " , ")</FormLabel>
               <FormControl>
@@ -102,14 +125,23 @@ const PostForm = ({ post }) => {
             </FormItem>
           )}
         />
+
         <div className="flex gap-4 items-center justify-end">
-            <Button type="button" className="shad-button_dark_4">Cancel</Button>
-            <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+          <Button
+            type="button"
+            className="shad-button_dark_4"
+            >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            className="shad-button_primary whitespace-nowrap"
+            >
+          </Button>
         </div>
-        <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default PostForm
+export default PostForm;
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,9 @@
 import { ID, Query } from "appwrite";
 
-import { INewUser } from "@/types";
-import { appwriteConfig, account, databases, avatars } from "./config";
+import { appwriteConfig, account, databases, avatars, storage } from "./config";
+import { INewPost, INewUser } from "@/types";
+import { error } from "console";
+import { useGetRecentPosts } from "../react-query/queries";
 
 // ============================================================
 // AUTH
@@ -69,10 +71,21 @@ export async function signInAccount(user: { email: string; password: string }) {
   }
 }
 
+// ============================== GET ACCOUNT
+export async function getAccount() {
+  try {
+    const currentAccount = await account.get();
+
+    return currentAccount;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // ============================== GET USER
 export async function getCurrentUser() {
   try {
-    // const currentAccount = await getAccount();
+    const currentAccount = await getAccount();
 
     if (!currentAccount) throw Error;
 
@@ -91,6 +104,7 @@ export async function getCurrentUser() {
   }
 }
 
+
 // ============================== SIGN OUT
 export async function signOutAccount() {
   try {
@@ -100,4 +114,110 @@ export async function signOutAccount() {
   } catch (error) {
     console.log(error)
   }
+}
+
+// ============================================================
+// POSTS
+// ============================================================
+
+// ============================== CREATE POST
+export async function createPost(post: INewPost) {
+  try {
+    // Upload file to appwrite storage
+    const uploadedFile = await uploadFile(post.file[0]);
+
+    if (!uploadedFile) throw Error;
+
+    // Get file url
+    const fileUrl = getFilePreview(uploadedFile.$id);
+    if (!fileUrl) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    // Convert tags into array
+    const tags = post.tags?.replace(/ /g, "").split(",") || [];
+
+    // Create post
+    const newPost = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      ID.unique(),
+      {
+        creator: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags: tags,
+      }
+    );
+
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    return newPost;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// ============================== UPLOAD FILE
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+
+    return uploadedFile;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// ============================== GET FILE URL
+export function getFilePreview(fileId: string) {
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      "top",
+      100
+    );
+
+    if (!fileUrl) throw Error;
+
+    return fileUrl;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// ============================== DELETE FILE
+export async function deleteFile(fileId: string) {
+  try {
+    await storage.deleteFile(appwriteConfig.storageId, fileId);
+
+    return { status: "ok" };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getRecentPosts() {
+  const posts = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.postCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)]
+  )
+
+  if(!posts) throw Error;
+
+  return posts;
 }
