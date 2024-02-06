720afeb4700af8dd0c3fb738d5962aece69768f8
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-06 08:32:19 -0600

    updated explore page

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -133,7 +133,8 @@ export async function createPost(post: INewPost) {
     }
 
     // Convert tags into array
-    const tags = post.tags?.replace(/ /g, "").split(",") || [];
+    const tags = typeof post.tags === 'string' ? post.tags.replace(/ /g, '').split(',') : [];
+
 
     // Create post
     const newPost = await databases.createDocument(
@@ -291,7 +292,8 @@ export async function updatePost(post: IUpdatePost) {
     }
 
     // Convert tags into array
-    const tags = post.tags?.replace(/ /g, "").split(",") || [];
+    const tags = typeof post.tags === 'string' ? post.tags.replace(/ /g, '').split(',') : [];
+
 
     //  Update post
     const updatedPost = await databases.updateDocument(
